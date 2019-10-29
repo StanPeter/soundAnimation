3d4ef26b0b7a1e93f59149da204ca918f7c6b5e8
stan05 <stanislav.petr7@gmail.com>
2019-10-30 00:32:40 +0100

    project finished

---
+++ data.js
@@ -0,0 +1,134 @@
+//data for index.html paperscript code
+
+var data = {
+	q: {
+		sound: new Howl({
+  		src: ['sounds/bubbles.mp3']
+		})
+	},
+	w: {
+		sound: new Howl({
+  		src: ['sounds/clay.mp3']
+		})
+	},
+	e: {
+		sound: new Howl({
+  		src: ['sounds/confetti.mp3']
+		})
+	},
+	r: {
+		sound: new Howl({
+  		src: ['sounds/corona.mp3']
+		})
+	},
+		t: {
+		sound: new Howl({
+  		src: ['sounds/dotted-spiral.mp3']
+		})
+	},
+	y: {
+		sound: new Howl({
+  		src: ['sounds/flash-1.mp3']
+		})
+	},
+	u: {
+		sound: new Howl({
+  		src: ['sounds/flash-2.mp3']
+		})
+	},
+	i: {
+		sound: new Howl({
+  		src: ['sounds/flash-3.mp3']
+		})
+	},
+	o: {
+		sound: new Howl({
+		src: ['sounds/glimmer.mp3']
+		})
+	},
+	p: {
+		sound: new Howl({
+  		src: ['sounds/moon.mp3']
+		})
+	},
+	a: {
+		sound: new Howl({
+  		src: ['sounds/pinwheel.mp3']
+		})
+	},
+	s: {
+		sound: new Howl({
+  		src: ['sounds/piston-1.mp3']
+		})
+	},
+		d: {
+		sound: new Howl({
+  		src: ['sounds/piston-2.mp3']
+		})
+	},
+	f: {
+		sound: new Howl({
+  		src: ['sounds/prism-1.mp3']
+		})
+	},
+	g: {
+		sound: new Howl({
+  		src: ['sounds/prism-2.mp3']
+		})
+	},
+	h: {
+		sound: new Howl({
+  		src: ['sounds/prism-3.mp3']
+		})
+	},
+	j: {
+		sound: new Howl({
+  		src: ['sounds/splits.mp3']
+		})
+	},
+	k: {
+		sound: new Howl({
+  		src: ['sounds/squiggle.mp3']
+		})
+	},
+	l: {
+		sound: new Howl({
+  		src: ['sounds/strike.mp3']
+		})
+	},
+	z: {
+		sound: new Howl({
+  		src: ['sounds/suspension.mp3']
+		})
+	},
+	x: {
+		sound: new Howl({
+  		src: ['sounds/timer.mp3']
+		})
+	},
+	c: {
+		sound: new Howl({
+  		src: ['sounds/ufo.mp3']
+		})
+	},
+	v: {
+		sound: new Howl({
+  		src: ['sounds/veil.mp3']
+		})
+	},
+	b: {
+		sound: new Howl({
+  		src: ['sounds/wipe.mp3']
+		})
+	},
+	n: {
+		sound: new Howl({
+		src: ['sounds/zig-zag.mp3']
+		})
+	},
+	m: {
+		sound: new Howl({
+  		src: ['sounds/moon.mp3']
+		})
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,14 @@
+A small sound animation project using Paper.js library and Howler.js library
+
+By pressing any key from data.js (basically from "a" to "z") will show a random color Circle animation
+which will scale until it dissappear + will produce a sound bounded to that key
+
+I tried to put all JS code to another file but could not sort out an error
+
+Also there will be Cross Origin error in the console which you can get rid
+of by running a local HTTP server
+--> for example on python3 in terminal " python -m http.server "
+which will open " http://localhost:8000 " local server
+
+
+Whoever can use these codes however he wishes
