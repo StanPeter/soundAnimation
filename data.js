f2fe1950b13ec6369a626cc687b351f7c9346d7f
StanPeter <stanislav.petr7@gmail.com>
2020-12-21 22:18:32 +0100

    added projectImage + description

--- data.js
+++ data.js
@@ -3,132 +3,132 @@
 var data = {
 	q: {
 		sound: new Howl({
-  		src: ['sounds/bubbles.mp3']
+			src: ['sounds/bubbles.mp3']
 		})
 	},
 	w: {
 		sound: new Howl({
-  		src: ['sounds/clay.mp3']
+			src: ['sounds/clay.mp3']
 		})
 	},
 	e: {
 		sound: new Howl({
-  		src: ['sounds/confetti.mp3']
+			src: ['sounds/confetti.mp3']
 		})
 	},
 	r: {
 		sound: new Howl({
-  		src: ['sounds/corona.mp3']
+			src: ['sounds/corona.mp3']
 		})
 	},
-		t: {
+	t: {
 		sound: new Howl({
-  		src: ['sounds/dotted-spiral.mp3']
+			src: ['sounds/dotted-spiral.mp3']
 		})
 	},
 	y: {
 		sound: new Howl({
-  		src: ['sounds/flash-1.mp3']
+			src: ['sounds/flash-1.mp3']
 		})
 	},
 	u: {
 		sound: new Howl({
-  		src: ['sounds/flash-2.mp3']
+			src: ['sounds/flash-2.mp3']
 		})
 	},
 	i: {
 		sound: new Howl({
-  		src: ['sounds/flash-3.mp3']
+			src: ['sounds/flash-3.mp3']
 		})
 	},
 	o: {
 		sound: new Howl({
-		src: ['sounds/glimmer.mp3']
+			src: ['sounds/glimmer.mp3']
 		})
 	},
 	p: {
 		sound: new Howl({
-  		src: ['sounds/moon.mp3']
+			src: ['sounds/moon.mp3']
 		})
 	},
 	a: {
 		sound: new Howl({
-  		src: ['sounds/pinwheel.mp3']
+			src: ['sounds/pinwheel.mp3']
 		})
 	},
 	s: {
 		sound: new Howl({
-  		src: ['sounds/piston-1.mp3']
+			src: ['sounds/piston-1.mp3']
 		})
 	},
-		d: {
+	d: {
 		sound: new Howl({
-  		src: ['sounds/piston-2.mp3']
+			src: ['sounds/piston-2.mp3']
 		})
 	},
 	f: {
 		sound: new Howl({
-  		src: ['sounds/prism-1.mp3']
+			src: ['sounds/prism-1.mp3']
 		})
 	},
 	g: {
 		sound: new Howl({
-  		src: ['sounds/prism-2.mp3']
+			src: ['sounds/prism-2.mp3']
 		})
 	},
 	h: {
 		sound: new Howl({
-  		src: ['sounds/prism-3.mp3']
+			src: ['sounds/prism-3.mp3']
 		})
 	},
 	j: {
 		sound: new Howl({
-  		src: ['sounds/splits.mp3']
+			src: ['sounds/splits.mp3']
 		})
 	},
 	k: {
 		sound: new Howl({
-  		src: ['sounds/squiggle.mp3']
+			src: ['sounds/squiggle.mp3']
 		})
 	},
 	l: {
 		sound: new Howl({
-  		src: ['sounds/strike.mp3']
+			src: ['sounds/strike.mp3']
 		})
 	},
 	z: {
 		sound: new Howl({
-  		src: ['sounds/suspension.mp3']
+			src: ['sounds/suspension.mp3']
 		})
 	},
 	x: {
 		sound: new Howl({
-  		src: ['sounds/timer.mp3']
+			src: ['sounds/timer.mp3']
 		})
 	},
 	c: {
 		sound: new Howl({
-  		src: ['sounds/ufo.mp3']
+			src: ['sounds/ufo.mp3']
 		})
 	},
 	v: {
 		sound: new Howl({
-  		src: ['sounds/veil.mp3']
+			src: ['sounds/veil.mp3']
 		})
 	},
 	b: {
 		sound: new Howl({
-  		src: ['sounds/wipe.mp3']
+			src: ['sounds/wipe.mp3']
 		})
 	},
 	n: {
 		sound: new Howl({
-		src: ['sounds/zig-zag.mp3']
+			src: ['sounds/zig-zag.mp3']
 		})
 	},
 	m: {
 		sound: new Howl({
-  		src: ['sounds/moon.mp3']
+			src: ['sounds/moon.mp3']
 		})
 	}
 }
--- readme.txt
+++ readme.txt
@@ -1,14 +1,27 @@
+Information:
+
 A small sound animation project using Paper.js library and Howler.js library
 
 By pressing any key from data.js (basically from "a" to "z") will show a random color Circle animation
 which will scale until it dissappear + will produce a sound bounded to that key
 
-I tried to put all JS code to another file but could not sort out an error
-
-Also there will be Cross Origin error in the console which you can get rid
-of by running a local HTTP server
---> for example on python3 in terminal " python -m http.server "
-which will open " http://localhost:8000 " local server
+Data(or better said variables) are inside data.js and JS logic inside index.html
 
 
-Whoever can use these codes however he wishes
+Instruction:
+
+    1. Open index.html in your browser(chrome preferred)
+    2. Press any key from A - Z in order to see sound animation
+
+
+
+
+Error handling ?:
+
+PS: There will be Cross Origin error in the console which you can simply ignore or 
+    get rid of by running a local HTTP server
+    --> for example with python3(+ I use ubuntu so Windows might differ) in terminal 
+    
+    " python -m http.server "
+
+    This will create " http://localhost:8000 " local server
